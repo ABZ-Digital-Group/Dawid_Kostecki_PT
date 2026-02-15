b3dacb6ae908b57f8a9b64e0110ce459365df793
stuart Keith <stuartiek@gmail.com>
2026-02-15 20:11:21 +0000

    Created Starter files and code

---
+++ server.js
@@ -0,0 +1,56 @@
+// CONNECT TO MONGO
+// const MongoClient = require('mongodb-legacy').MongoClient;
+// const url = 'mongodb://127.0.0.1:27017';
+// const client = new MongoClient(url);
+// const dbname = 'hack';
+
+// LOAD NPM PACKAGES
+let express = require('express');
+let session = require('express-session');
+let bodyParser = require('body-parser');
+const {Console, profile} = require('console');
+const app = express();
+
+app.use(session({secret: 'example'}));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.set('view engine', 'ejs');
+
+// CONNECT TO DB
+let db;
+connectDB();
+async function connectDB(){
+    await client.connect();
+    console.log('Connected Successfully to Server');
+    db = client.db(dbname);
+    app.listen(8080);
+    console.log('Connected to Port: 8080');
+};
+
+// RENDER PAGES
+
+
+// INDEX PAGE
+app.get('/', function(req, res){
+    res.render('pages/index');
+});
+
+app.get('/dataform', function(req, res){
+    if(!req.session.loggedin){res.redirect('/');return;}
+    res.render('pages/dataform');
+});
+
+app.get('/signup', function(req, res){
+    res.render('pages/signup');
+});
+
+
+
+//LOGOUT
+// app.get('/logout', function(req, res){
+//     req.session.loggedin = false;
+//     req.session.destroy();
+//     res.redirect('/');
+// });
